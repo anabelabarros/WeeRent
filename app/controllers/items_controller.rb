c36690d056f89a08d0687426d65a024808f0fa71
Glory Kinsiedi-Matonga <glory.matong893@yahoo.co.uk>
2024-05-09 15:50:42 +0100

    Adding to the buttons and styling to the index

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,24 @@
 class ItemsController < ApplicationController
-  def index
+  def items
     @items = Item.all
   end
 
   def show
     @item = Item.find(params[:id])
   end
+
+  def sleep
+    @items= Item.where(category: "sleep")
+  end
+  def travel
+    @items= Item.where(category: "travel")
+  end
+  def play
+    @items= Item.where(category: "play")
+  end
+  def feeding
+    @items= Item.where(category: "feeding")
+  end
+
 
 end
