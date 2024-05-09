fdeaaee49776ac43b1436f1de6d4c286a79a7a07
marita jeyarajan-davidsson <atiram23@gmail.com>
2024-05-09 15:24:06 +0100

    created: 2nd user journey with new+create

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,4 +7,36 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    @item.user = current_user
+    if @item.save
+      redirect_to @item, notice: 'Product was sucessfully listed'
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to @item, notice: 'Product was successfully updated'
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  # Only allow a trusted parameter "white list" through.
+  def item_params
+    params.require(:item).permit(:name, :description, :price, :category)
+  end
 end
